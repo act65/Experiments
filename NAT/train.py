6b27ebcf2d6103938765ed17c755bd0b208e3abd
act65 <te1farac@gmail.com>
2017-05-03 10:13:31 +1200

    more progress. just sorting train script

--- NAT/train.py
+++ NAT/train.py
@@ -29,44 +29,7 @@ def batch(ims, labels, batchsize):
 def accuracy(p, y):
     return tf.reduce_mean(tf.cast(tf.equal(tf.argmax(p, axis=1), y), tf.float32))
 
-
-def validate(sess, train_step, ims, labels):
-    """
-    Given that we are doing unsupervised pretraining for a discrimination task,
-    it makes sense to validate our model on discrimination.
-    """
-    # reset
-    inits = [var.initializer for var in tf.get_collection('TRAINABLE_VARIABLES',
-                                                          scope='discrimination')]
-    sess.run(inits)
-
-    # train classifier (only)
-    for i, batch_ims, batch_labels in batch(ims, labels, FLAGS.batchsize):
-        sess.run(train_step, {x: batch_ims, T: batch_labels})
-
-
-def encoder(x):
-    with tf.variable_scope('encoder'):
-        # channel_sizes = [(64, 2), (32, 2), (16, 2)]
-        channel_sizes = [(16, 1)] * 10
-        with slim.arg_scope([slim.conv2d],
-                            activation_fn=tf.nn.relu,
-                            weights_initializer=tf.orthogonal_initializer(),
-                            biases_initializer=tf.constant_initializer(0.0)):
-
-            fmap = slim.stack(x, slim.conv2d, [(k, (3, 3), (s, s), 'SAME')
-                                            for k, s in channel_sizes])
-        return fmap
-
-def classifier(rep):
-    with tf.variable_scope('classifier'):
-        logits = slim.fully_connected(tf.reduce_mean(fmap, axis=[1,2]), 10,
-                             activation_fn=None)
-        return logits
-
-
 def main(_):
-    print('Get data')
     mnist = input_data.read_data_sets(FLAGS.datadir, one_hot=False)
     ims = np.reshape(mnist.train.images, [-1, 28, 28, 1]).astype(np.float32)
     labels = np.reshape(mnist.train.labels, [-1]).astype(np.int64)
@@ -77,58 +40,87 @@ def main(_):
     x = tf.placeholder(shape=[50, 28, 28, 1], dtype=tf.float32)
     T = tf.placeholder(shape=[50], dtype=tf.int64)
 
+    # set up
     global_step = tf.Variable(0, name='global_step', trainable=False)
     step_summary = tf.summary.scalar('global_step', global_step)
     opt = tf.train.AdamOptimizer(FLAGS.lr)
 
+    def encoder(x):
+        with tf.variable_scope('encoder'):
+            channel_sizes = [(16, 2), (32, 2), (64, 2)]
+            with slim.arg_scope([slim.conv2d],
+                                activation_fn=tf.nn.relu,
+                                weights_initializer=tf.orthogonal_initializer(),
+                                biases_initializer=tf.constant_initializer(0.0)):
+
+                return slim.stack(x, slim.conv2d, [(k, (3, 3), (s, s), 'SAME')
+                                                for k, s in channel_sizes])
+
+    def classifier(x):
+        with tf.variable_scope('classifier'):
+            return slim.fully_connected(tf.reduce_mean(x, axis=[1,2]), 10,
+                                 activation_fn=None)
+
+    # parameterised functions
     fmap = encoder(x)
     logits = classifier(fmap)
 
+    # losses
     unsupervised_loss = nat(fmap)
     discrim_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=T)
 
-
+    # optimisation steps
     pretrain_step = opt.minimize(unsupervised_loss, global_step=global_step,
                               var_list=encoder_vars)
     train_step = opt.minimize(discrim_loss, var_list=discrim_vars)
 
+    # metrics
     p = tf.nn.softmax(logits)
-    acc = accuracy(p, T)
+    acc = accuracy(p, T)  # TODO. switch to streaming accuracy
 
     # summaries
     loss_summary = tf.summary.merge([tf.summary.scalar(loss.name, loss)
                                      for loss in [unsupervised_loss, discrim_loss]])
     accuracy_summary = tf.summary.scalar('acc', acc)
 
+    def train(sess, writer, batch_ims):
+        step, L, _ = sess.run([global_step, loss, pretrain_step], {x: batch_ims})
+        print('\rloss: {:.3f}'.format(L), end='', flush=True)
+        writer.add_summary(loss_summ, step)
+        return step
 
-    n = np.sum([np.prod(var.get_shape().as_list())
-               for var in tf.trainable_variables()])
-    print('num of vars {}'.format(n))
+    def validate(sess, writer):
+        """
+        Given that we are doing unsupervised pretraining for a discrimination task,
+        it makes sense to validate our model on discrimination.
+        """
+        # reset
+        inits = [var.initializer for var in tf.get_collection('TRAINABLE_VARIABLES',
+                                                              scope='classifier')]
+        sess.run(inits)  # also need to reset metrics
+
+        # train classifier (only) on a labelled subset of the data
+        for i, batch_ims, batch_labels in batch(ims, labels, FLAGS.batchsize):
+            sess.run(train_step, {x: batch_ims, T: batch_labels})
+
+        for i, batch_ims, batch_labels in batch(test_ims, test_labels,
+                                                FLAGS.batchsize):
+            sess.run(acc_op)
+
+        writer.add_summary(sess.run(acc), step)
+
 
     with tf.Session() as sess:
-        writer = tf.summary.FileWriter(logdir, sess.graph)
-        run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-        run_metadata = tf.RunMetadata()
-
+        writer = tf.summary.FileWriter(FLAGS.logdir, sess.graph)
         sess.run(tf.global_variables_initializer())
-        step = 0
+
         for e in range(FLAGS.epochs):
-            for i, batch_ims, batch_labels in batch(ims, labels, FLAGS.batchsize):
-                L, _ = sess.run([loss, train_step], {x: batch_ims},
-                                options=run_options, run_metadata=run_metadata)
-                print('\rloss: {:.3f}'.format(L), end='', flush=True)
+            for _, batch_ims, batch_labels in batch(ims, labels, FLAGS.batchsize):
+                step = train(sess, writer, batch_ims)
 
-                tl = timeline.Timeline(run_metadata.step_stats)
-                ctf = tl.generate_chrome_trace_format()
-                with open(os.path.join(logdir, 'timeline.json'), 'w') as f:
-                    f.write(ctf)
-
-                if step%500==0:
+                if step%50==0:
                     # validate and save summaries
-                    writer.add_summary(train_summ, step)
-                    writer.add_summary(loss_summ, step)
-                    writer.add_summary(test_summ, step)
-                step += 1
+                    validate(sess, writer)
 
 if __name__ == '__main__':
     tf.app.run()
